419f13f6f38e14e0165d643eda3c564c8c79a8b5
Katherine Crowson <crowsonkb@gmail.com>
2019-01-06 16:43:38 -0800

    Add detailed docstring to pp_highlighting.examples.calc.lassoc_mapreduce

--- pp_highlighting/examples/calc.py
+++ pp_highlighting/examples/calc.py
@@ -10,7 +10,29 @@ from .repl import repl
 
 
 def lassoc_mapreduce(func):
-    """Parses left-associative binary operators without left recursion."""
+    """Parses left-associative binary operators without left recursion.
+
+    Given a mapping function :func:`func`, returns a function which takes a
+    sequence of alternating operands and operators and carries out the given
+    operations left to right. The EBNF for producing such a list is:
+    :code:`expr = operand , { operator , operand } ;`. :func:`lassoc_mapreduce`
+    can be used to generate a parse action for a parse expression implementing
+    this EBNF.
+
+    Examples:
+
+        >>> mapping = lambda op: {'+': operator.add, '-': operator.sub}[op]
+        >>> expr = ppc.integer + pp.ZeroOrMore(pp.oneOf('+ -') + ppc.integer)
+        >>> expr.addParseAction(lassoc_mapreduce(mapping))
+        >>> expr.parseString('10 + 10 - 3')[0]
+        17
+
+    Args:
+        func (Callable[[T], Callable[[T2, T3], T2]]): The mapping function.
+
+    Returns:
+        Callable[[Sequence[Union[T, T2, T3]]], T2]: The parse action.
+    """
     def reducer(t):
         accum = t[0]
         for i in range(2, len(t), 2):
