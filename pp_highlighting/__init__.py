872b122a4ced72de1b58c6e549d8ddf36abf7643
Katherine Crowson <crowsonkb@gmail.com>
2019-01-09 12:40:36 -0800

    Version 0.1.1

--- pp_highlighting/__init__.py
+++ pp_highlighting/__init__.py
@@ -4,4 +4,5 @@ from .pp_highlighter import dummy_styler, PPHighlighter
 from .pp_validator import PPValidator
 
 __all__ = ['dummy_styler', 'PPHighlighter', 'PPValidator']
-__version__ = '0.1.0'
+
+__version__ = '0.1.1'
