848383137633a6ae60fe9a108322f6897cc13603
Katherine Crowson <crowsonkb@gmail.com>
2019-01-26 08:26:34 -0800

    Add more StyledElement unit tests

--- tests/test_styled_element.py
+++ tests/test_styled_element.py
@@ -1,6 +1,6 @@
 """Unit tests for pp_highlighter.StyledElement."""
 
-# pylint: disable=missing-docstring, protected-access, too-many-public-methods
+# pylint: disable=missing-docstring
 
 import unittest
 
@@ -11,14 +11,26 @@ from pp_highlighting.pp_highlighter import StyledElement
 
 class TestStyledElement(unittest.TestCase):
     def test_basic(self):
-        s = 'abc'
         fragments = {}
-        style = 'class:abc'
-        expr = pp.Literal(s)
-        s_expr = StyledElement(fragments, style, expr)
-        s_expr.parseString(s, parseAll=True)
+        expr = pp.Literal('abc')
+        s_expr = StyledElement(fragments, 'class:abc', expr)
+        s_expr.parseString('abc', parseAll=True)
         self.assertEqual(list(fragments), [0])
-        self.assertEqual(fragments[0], (style, s))
+        self.assertEqual(fragments[0], ('class:abc', 'abc'))
+
+    def test_str(self):
+        expr = pp.Literal('abc')
+        s_expr = StyledElement({}, 'class:abc', expr)
+        self.assertEqual(str(expr), str(s_expr))
+
+    def test_does_parse_actions(self):
+        fragments = {}
+        expr = pp.Word(pp.nums).addParseAction(lambda t: int(t[0]))
+        expr.addParseAction(lambda t: t[0] * 2)
+        s_expr = StyledElement(fragments, 'class:abc', expr)
+        result = s_expr.parseString('123', parseAll=True)[0]
+        self.assertEqual(fragments[0], ('class:abc', '123'))
+        self.assertEqual(result, 246)
 
 
 if __name__ == '__main__':
